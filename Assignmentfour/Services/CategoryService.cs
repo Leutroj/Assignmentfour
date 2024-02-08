419d681bd02d247e1453a26b6260974f5f949e41
Alexander Leu <leutroj@gmail.com>
2024-02-08 16:54:15 +0100

    CategoryService

---
+++ Assignmentfour/Services/CategoryService.cs
@@ -0,0 +1,54 @@
+﻿using Assignmentfour.Entities;
+using Assignmentfour.Repositories;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignmentfour.Services
+{
+    internal class CategoryService
+    {
+        private readonly CategoryRepository _categoryRepository;
+
+        public CategoryService(CategoryRepository categoryRepository) 
+        {
+            _categoryRepository = categoryRepository;   
+        }
+
+
+        public CategoryEntity CreateCategory(string categoryName) 
+        {
+            var categoryEntity = _categoryRepository.Get(x => x.CategoryName == categoryName);
+            categoryEntity ??= _categoryRepository.Create(new CategoryEntity { CategoryName = categoryName });
+
+            return categoryEntity;
+        }
+
+        public CategoryEntity GetCategoryById(int id)
+        {
+            var categoryEntity = _categoryRepository.Get(x => x.Id == id);
+            return categoryEntity;
+        }
+
+        public IEnumerable<CategoryEntity> GetCategories() 
+        {
+            var categories = _categoryRepository.GetAll();
+            return categories;
+        }
+
+        public CategoryEntity UpdateCategory(CategoryEntity categoryEntity) 
+        {
+            var updatedCategoryEntity = _categoryRepository.Update(x => x.Id == categoryEntity.Id, categoryEntity);
+            return updatedCategoryEntity;
+        }
+
+        public void DeleteCategory(int id) 
+        {
+            _categoryRepository.Delete(x => x.Id == id);    
+        }
+
+    }
+}
