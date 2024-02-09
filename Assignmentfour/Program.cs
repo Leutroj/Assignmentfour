76bef93e2196f1112ba711c3dbbee555612ef549
Alexander Leu <leutroj@gmail.com>
2024-02-09 22:49:59 +0100

    AddedPath

--- Assignmentfour/Program.cs
+++ Assignmentfour/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Assignmentfour.Contexts;
+using Assignmentfour.Repositories;
+using Assignmentfour.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -9,5 +11,16 @@ var builder = Host.CreateDefaultBuilder().ConfigureServices(services =>
 
     services.AddDbContext<DataContext>(x => x.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Projects\Assignmentfour\Assignmentfour\Data\database.mdf;Integrated Security=True;Connect Timeout=30"));
 
+    services.AddScoped<AddressRepository>();
+    services.AddScoped<CategoryRepository>();
+    services.AddScoped<RoleRepository>();
+    services.AddScoped<ProductRepository>();
+    services.AddScoped<CustomerRepository>();
+
+    services.AddScoped<AddressService>();
+    services.AddScoped<CategoryService>();
+    services.AddScoped<RoleService>();
+    services.AddScoped<ProductService>();
+    services.AddScoped<CustomerService>();
 
 }).Build();
