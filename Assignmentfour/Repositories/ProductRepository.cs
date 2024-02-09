798a1126acfbf1fd2242dccffe6a01d47c0401ff
Alexander Leu <leutroj@gmail.com>
2024-02-09 22:45:56 +0100

    OverRide2

--- Assignmentfour/Repositories/ProductRepository.cs
+++ Assignmentfour/Repositories/ProductRepository.cs
@@ -1,12 +1,37 @@
 ﻿using Assignmentfour.Contexts;
 using Assignmentfour.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace Assignmentfour.Repositories
 {
     internal class ProductRepository : Repo<ProductEntity>
     {
+        private readonly DataContext _context;
         public ProductRepository(DataContext context) : base(context)
         {
+            _context = context;
+
+
+
+
+        }
+
+        public override ProductEntity Get(Expression<Func<ProductEntity, bool>> expression)
+        {
+            var entity = _context.Products
+                 .Include(i => i.Category)
+                 .FirstOrDefault(expression);
+
+            return entity!;
+        }
+
+        public override IEnumerable<ProductEntity> GetAll()
+        {
+            return _context.Products
+                .Include(i => i.Category)
+                .ToList();
         }
     }
 }
