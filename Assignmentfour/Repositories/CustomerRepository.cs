dd50c33aee7e1b270658d0b4cb54ca7abe1a82e7
Alexander Leu <leutroj@gmail.com>
2024-02-09 17:12:35 +0100

    overirdesstart

--- Assignmentfour/Repositories/CustomerRepository.cs
+++ Assignmentfour/Repositories/CustomerRepository.cs
@@ -1,12 +1,35 @@
 ﻿using Assignmentfour.Contexts;
 using Assignmentfour.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Assignmentfour.Repositories
 {
     internal class CustomerRepository : Repo<CustomerEntity>
     {
+        private readonly DataContext _context;
         public CustomerRepository(DataContext context) : base(context)
         {
+            _context = context;
+        }
+
+        public override CustomerEntity Get(Expression<Func<CustomerEntity, bool>> expression)
+        {
+            var entity = _context.Customers
+                .Include(i => i.Address)
+                .Include(i => i.Role)
+                .FirstOrDefault(expression);
+
+            return entity!;
+        }
+
+        public override IEnumerable<CustomerEntity> GetAll()
+        {
+            return _context.Customers
+                .Include(i => i.Address)
+                .Include(i => i.Role)
+                .ToList();
         }
     }
 }
